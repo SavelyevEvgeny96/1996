57f2697a2ca4c09c33896acc9711dd5f0b25a068
rodnoy96 <Zhenyasavelyev08494@gmail.com>
2022-10-23 22:06:51 +0300

    push

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -3,22 +3,17 @@ package jm.task.core.jdbc.dao;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 import org.hibernate.Session;
-
-
-import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
-
+import org.hibernate.SessionFactory;
 import java.util.List;
 
 
 public class UserDaoHibernateImpl implements UserDao {
+    private final SessionFactory sessionFactory=Util.getSessionFactory();
     private static final String CREAT_USER_TABLE = "CREATE TABLE IF NOT EXISTS human " +
             "(ID BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY, " +
             "name varchar(40) NOT NULL, " +
             "lastname varchar(40) NOT NULL, age TINYINT)";
-    private static final String CLEAN_TABLE = "DELETE FROM human";
+    private static final String CLEAN_TABLE = "TRUNCATE TABLE human";
     private static final String DROP_USER_TABLE = "DROP TABLE IF EXISTS human";
 
 
@@ -29,7 +24,7 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
-        try (Session session = Util.getSessionFactory().openSession()) {
+        try (Session session = sessionFactory.openSession()) {
             session.beginTransaction();
             session.createSQLQuery(CREAT_USER_TABLE).executeUpdate();
             session.getTransaction().commit();
@@ -39,7 +34,7 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void dropUsersTable() {
-        try (Session session = Util.getSessionFactory().openSession()) {
+        try (Session session = sessionFactory.openSession()) {
             session.beginTransaction();
             session.createSQLQuery(DROP_USER_TABLE).executeUpdate();
             session.getTransaction().commit();
@@ -49,7 +44,7 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-        try (Session session = Util.getSessionFactory().openSession()) {
+        try (Session session = sessionFactory.openSession()) {
             session.beginTransaction();
             User newUser = new User(name, lastName, age);
             session.save(newUser);
@@ -61,7 +56,7 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void removeUserById(long id) {
-        try (Session session = Util.getSessionFactory().openSession()) {
+        try (Session session =sessionFactory.openSession()) {
             session.beginTransaction();
             User newUser = session.get(User.class, id);
             if (newUser != null)
@@ -75,28 +70,11 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public List<User> getAllUsers() {
-        try (Session session = Util.getSessionFactory().openSession()) {
-            session.beginTransaction();
-
-            CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
-            CriteriaQuery<User> criteriaQuery = criteriaBuilder.createQuery(User.class);
-            Root<User> userRoot = criteriaQuery.from(User.class);
-            CriteriaQuery<User> all = criteriaQuery.select(userRoot);
-
-            TypedQuery<User> allQuery = session.createQuery(all);
-            List<User> result = allQuery.getResultList();
-            session.getTransaction();
-
-            return result;
-        }
-
-
+        return (List<User>) sessionFactory.openSession().createQuery("SELECT u FROM User u ", User.class).list();
     }
-
-
     @Override
     public void cleanUsersTable() {
-        try (Session session = Util.getSessionFactory().openSession()) {
+        try (Session session = sessionFactory.openSession()) {
             session.createSQLQuery(CLEAN_TABLE).executeUpdate();
             session.getTransaction().commit();
 
